48f036f547732aec5374e3f40d67167e1f47130c
Nemnem2 <68030615+Nemnem2@users.noreply.github.com>
2023-10-23 12:53:33 +0300

    Update if condition check vowels.c

--- if condition check vowels.c
+++ if condition check vowels.c
@@ -2,7 +2,7 @@
 
 int main() {
     char c;
-    printf("Please neter a character\n");
+    printf("Please enter a character\n");
     scanf("%c",&c);
     if ((c== 'a') || (c == 'e') || (c == 'o') || (c == 'i') || (c == 'u')){
         printf("This chaeracter is a vowel\n");
